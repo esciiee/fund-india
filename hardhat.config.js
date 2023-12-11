b8437e3870addbe2c01a40f83aae7db4e447fed3
suraj <surajj3404@gmail.com>
2023-12-12 03:12:22 +0530

    Update Config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,39 @@
-require("dotenv").config()
 require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-    solidity: "^0.8.8",
+    solidity: "0.8.8",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY],
+        },
+        localhost: {
+            url: process.env.LOCALHOST_RPC_URL,
+            chainId: 31337,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+        token: "MATIC"
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+        }
+    }
 }
