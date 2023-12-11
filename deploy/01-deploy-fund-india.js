a64e303e10c1f1f094527e5847a55be6ab416425
suraj <surajj3404@gmail.com>
2023-12-12 03:12:48 +0530

    Create Contracts

---
+++ deploy/01-deploy-fund-india.js
@@ -0,0 +1,8 @@
+const { parseEther } = require("ethers")
+const { network } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+}
